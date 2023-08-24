c6ac432b205ab2b10c2d286d3389e1876456375c
mhkh <96872737+mhkh1400@users.noreply.github.com>
2023-08-24 14:02:57 +0200

    Update juicity-installer.sh

--- juicity-installer.sh
+++ juicity-installer.sh
@@ -135,7 +135,7 @@ read -p "Enter SNI (or press enter to www.speedtest.net): " SNI
 [[ -z "$SNI" ]] && SNI=$(www.speedtest.net)
 # Generate keys
 openssl ecparam -genkey -name prime256v1 -out "$INSTALL_DIR/private.key"
-openssl req -new -x509 -days 36500 -key "$INSTALL_DIR/private.key" -out "$INSTALL_DIR/fullchain.cer" -subj "$SNI"
+openssl req -new -x509 -days 36500 -key "$INSTALL_DIR/private.key" -out "$INSTALL_DIR/fullchain.cer" -subj "/CN=$SNI"
 
 cat > $CONFIG_FILE <<EOL
 {
